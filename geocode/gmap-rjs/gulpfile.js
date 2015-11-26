b3527e440a2423c1c7c7d277dd66a4f0c6443fc1
jimfrenette <jimfrenette@yahoo.com>
2015-11-26 12:35:57 -0500

    added BrowserSync static server

--- geocode/gmap-rjs/gulpfile.js
+++ geocode/gmap-rjs/gulpfile.js
@@ -1,12 +1,23 @@
 'use strict';
-
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
+// copy vendor libraries into app
 gulp.task('copy', function() {
-    // copy vendor libraries into app
     gulp.src([
         './bower_components/requirejs-plugins/src/async.js',
         './node_modules/requirejs/require.js'
         ])
         .pipe(gulp.dest('./src/js/require/'));
+});
+
+// BrowserSync static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: './',
+            directory: false,
+            index: 'index.html'
+        }
+    });
 });
