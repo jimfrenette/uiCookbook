a4666b55fc86d63d13f9715a2597b4d5434cae6a
U-VGN-FW290\admin <jimfrenette@yahoo.com>
2016-04-01 18:17:28 -0400

    add watchify, protocol removed from api reequest

--- geocode/gmap-browserify/gulpfile.js
+++ geocode/gmap-browserify/gulpfile.js
@@ -1,38 +1,51 @@
 'use strict';
 
 var gulp        = require('gulp'),
+    assign      = require('lodash.assign'),
     browserify  = require('browserify'),
     buffer      = require('vinyl-buffer'),
     gutil       = require('gulp-util'),
     source      = require('vinyl-source-stream'),
     sourcemaps  = require('gulp-sourcemaps'),
     uglify      = require('gulp-uglify'),
+    watchify    = require('watchify'),
     webserver   = require('gulp-webserver');
 
-gulp.task('bundle', function () {
-  var b = browserify({
-    entries: './src/js/index.js',
+// custom browserify options
+var customOpts = {
+    entries: ['./src/js/index.js'],
     debug: true
-  });
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
 
+gulp.task('js', bundle); // run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
   return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('bundle.js'))
+    // optional, remove if you don't need to buffer file contents
     .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(uglify())
-        .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./js/'));
-});
+    // optional, remove if you dont want sourcemaps
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+    // Add transformation tasks to the pipeline here.
+    .pipe(uglify()) // minify
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('./js'));
+}
 
 // static web server w/ livereload
 gulp.task('server', function() {
     gulp.src('./')
         .pipe(webserver({
-            livereload: true,
+            port: 3000,
             directoryListing: false,
             open: true
         }));
 });
 
-gulp.task('default', ['bundle','server']);
+gulp.task('default', ['js', 'server']);
--- geocode/gmap-browserify/src/js/modules/gapi.js
+++ geocode/gmap-browserify/src/js/modules/gapi.js
@@ -1,6 +1,6 @@
 var jsonp = require('./jsonp');
 
-var gapiurl = 'http://maps.googleapis.com/maps/api/js?callback=__googleMapsApiOnLoadCallback';
+var gapiurl = '//maps.googleapis.com/maps/api/js?callback=__googleMapsApiOnLoadCallback';
 
 exports.load = function (done) {
     jsonp(gapiurl, '__googleMapsApiOnLoadCallback', done);
