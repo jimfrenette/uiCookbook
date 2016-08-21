2f5fbbed09381a49b493ba355fcb7d7b40eff459
U-VGN-FW290\admin <jimfrenette@yahoo.com>
2016-08-21 18:57:19 -0400

    compare custom handlebars helper

--- hb-browserify/src/js/index.js
+++ hb-browserify/src/js/index.js
@@ -1,4 +1,46 @@
 var data = require('./modules/data');
 var list = require('../hbs/people.hbs');
 
+var Handlebars = require("hbsfy/runtime");
+
+Handlebars.registerHelper('compare', function (lvalue, operator, rvalue, options) {
+
+    var operators, result;
+
+    if (arguments.length < 3) {
+        throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+    }
+
+    if (options === undefined) {
+        options = rvalue;
+        rvalue = operator;
+        operator = "===";
+    }
+
+    operators = {
+        '==': function (l, r) { return l == r; },
+        '===': function (l, r) { return l === r; },
+        '!=': function (l, r) { return l != r; },
+        '!==': function (l, r) { return l !== r; },
+        '<': function (l, r) { return l < r; },
+        '>': function (l, r) { return l > r; },
+        '<=': function (l, r) { return l <= r; },
+        '>=': function (l, r) { return l >= r; },
+        'typeof': function (l, r) { return typeof l == r; }
+    };
+
+    if (!operators[operator]) {
+        throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
+    }
+
+    result = operators[operator](lvalue, rvalue);
+
+    if (result) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+
+});
+
 document.querySelector('#people').innerHTML = list(data);
