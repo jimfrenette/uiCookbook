8d8468ee452d3bac1c061c79a64d07cfdcbb1e34
Jim <jimfrenette@yahoo.com>
2018-01-23 19:33:40 -0500

    slick js

--- sass-cssnano/webpack/webpack.config.js
+++ sass-cssnano/webpack/webpack.config.js
@@ -1,43 +1,45 @@
 const path = require('path')
 const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  context: path.resolve(__dirname, './src'),
+context: path.resolve(__dirname, './src'),
   entry: {
-    /* app: './js/index.js',*/
-    css: './sass/main.scss',
+    app: './js/index.js'
   },
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
     {
-      test: /\.(css|scss)$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-            {
-              loader: 'css-loader',
-              options: {
-                minimize: true || {/* CSSNano Options */}
-              }
-            },
-            {
-              loader: 'postcss-loader'
-            },
-            {
-              loader: 'sass-loader'
-            }
-        ]
-      })
-    }]
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }
+    ]
   },
-  plugins: [
-    new ExtractTextPlugin('style.css'),
-  ]
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
 }
 
