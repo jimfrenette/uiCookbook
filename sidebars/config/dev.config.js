44ae7beeb26e182fcbd9312d6162efac69d869ba
Jim <jimfrenette@yahoo.com>
2018-09-10 17:59:37 -0400

    webpack-dev-server with write file plugin

--- sidebars/config/dev.config.js
+++ sidebars/config/dev.config.js
@@ -1,6 +1,10 @@
 const baseConfig = require('./base.config.js');
 const merge = require('webpack-merge');
+const writeFilePlugin = require('write-file-webpack-plugin');
 
 module.exports = merge(baseConfig, {
-  devtool: 'eval-source-map'
+  devtool: 'eval-source-map',
+  plugins: [
+    new writeFilePlugin()
+  ]
 });
