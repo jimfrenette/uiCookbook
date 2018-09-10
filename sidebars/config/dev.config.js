07d8cadc4eedbb935401b731ca1c49e4fbef177d
Jim <jimfrenette@yahoo.com>
2018-09-10 15:49:49 -0400

    remove webpack-dev-server

--- sidebars/config/dev.config.js
+++ sidebars/config/dev.config.js
@@ -2,5 +2,5 @@ const baseConfig = require('./base.config.js');
 const merge = require('webpack-merge');
 
 module.exports = merge(baseConfig, {
-  devtool: 'eval-source-map',
+  devtool: 'eval-source-map'
 });
